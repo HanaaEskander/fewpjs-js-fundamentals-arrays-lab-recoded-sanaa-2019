90a03b0f6fb27e00d491dfb6fa3e5508dfc3300e
Hana'a Eskander <hanaa.eskander95@gmail.com>
2019-11-30 06:54:34 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,3 +1,5 @@
 // Write your solution here!
 let append =["Milo", "Otis", "Garfield"];
 let preappend =["Milo", "Otis", "Garfield"];
+let removeLast= ["Milo", "Otis", "Garfield"];
+let removeFirst= ["Milo", "Otis", "Garfield"];
